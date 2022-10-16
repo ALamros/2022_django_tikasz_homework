ddb79ea3d411d470334243b060c36712fd88b856
ALamros <tikasz.zsolt.szeb@hotmail.com>
2022-10-16 18:31:40 +0200

    implement model and hard replace coded posts to db

--- test_page/tpage/views.py
+++ test_page/tpage/views.py
@@ -1,28 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import PostsModel
 
-posts = [
-    {
-        'author': 'Tikász Zsolt',
-        'title': 'Blog post 01',
-        'content': 'First content',
-        'date_posted': '2022.oktober 16.'
-    },
-    {
-        'author': 'Tikász Zsolt',
-        'title': 'Blog post 02',
-        'content': '2. content',
-        'date_posted': '2022.oktober 17.'
-    },
-    {
-        'author': 'Tikász Zsolt',
-        'title': 'Blog post 03',
-        'content': 'Asd content',
-        'date_posted': '2022.oktober 18.'
-    }
-]
+
 
 def fpage(request):
+    posts = PostsModel.objects.all()
     context = {
         'posts': posts
     }
